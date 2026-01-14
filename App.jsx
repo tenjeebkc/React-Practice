7c79da96ce9107b10d71afabfa734f95a7323c02
Tenjeeb K. C. <129965142+tenjeebkc@users.noreply.github.com>
2026-01-14 21:28:06 +0545

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const isHot = true
+
+  return (
+    <>
+    <h2>{isHot? "Hot weather" : "Not so hot weather"}</h2>
+    </>
+  )
+}
+
+export default App
